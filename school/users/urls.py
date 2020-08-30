0fe224ba38b93487c96f50f8fce5d70eb42ef8a8
avison <avisonlivingstone@gmail.com>
2020-08-30 23:15:39 +0200

    profile page commit

--- school/users/urls.py
+++ school/users/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login')
+    path('', views.login, name='login'),
+    path('profile/', views.profile, name= 'profile')
 ]
--- school/users/views.py
+++ school/users/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def login(request):
 	return render(request, 'users/login.html')
+
+
+def profile(request):
+	return render(request, 'users/profile.html', {'tag': 'Profile Page'})
