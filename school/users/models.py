15078f6847465408f84bb5fcf24a5e558b036f67
avison <avisonlivingstone@gmail.com>
2020-09-01 18:09:12 +0200

    Database Model

--- school/users/models.py
+++ school/users/models.py
@@ -1,4 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+	first_name = models.CharField(max_length=150)
+	last_name = models.CharField(max_length=150)
+	D_O_B = models.DateTimeField()
+	address = models.CharField()
+	student_class = models.IntegerField()
+	student_club = models.CharField()
 
+
+	def fullname(self, first_name, last_name):
+		return f'{self.first_name}{self.last_name}'
+
+
+	def __str__(self):
+		return self.fullname()
+
+class Teacher(models.Model):
+	first_name = models.CharField(max_length=150)
+	last_name = models.CharField(max_length=150)
+	
