68a0c485d76c5944fdf212afe2d70768bc4d6253
avison <avisonlivingstone@gmail.com>
2020-09-04 20:55:59 +0200

    Student Logic Cont

--- school/users/models.py
+++ school/users/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from model_utils import Choices
 
-class Student(models.Model):
+class Senior_Student(models.Model):
+	SUBJECTS = Choices(
+		('Mathematics',_('Mathematics')),
+		('English',_('English Language')),
+		('Physics',_('Physics')),
+		('Chemistry',_('Chemistry')),
+		('Biology',_('Biology')),
+		('Geography',_('Geography')),
+		('Accounts',_('Financial Accounting')),
+		('Commerce',_('Commerce')),
+		('Literature',_('Literature in English')),
+		('FMath',_('Further Mathematics'))
+	)
 	first_name = models.CharField(max_length=150)
 	last_name = models.CharField(max_length=150)
 	username = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -9,21 +22,18 @@ class Student(models.Model):
 	address = models.CharField(max_length=300)
 	student_class = models.IntegerField()
 	student_club = models.CharField(max_length=100)
-	
-
-
-	def fullname(self, first_name, last_name):
-		return f'{self.first_name}{self.last_name}'
+	subject = models.CharField(max_length=50, choices=SUBJECTS)
 
 
 	def __str__(self):
-		return self.fullname()
+		return f'{self.first_name}{self.last_name}'
 
 class Teacher(models.Model):
+	username = models.ManyToManyFields()
 
 
 	class Meta:
 		model = Student
-		fields = ['first_name','last_name','address']
+		fields = ['first_name','last_name','address','subject']
 
 	
