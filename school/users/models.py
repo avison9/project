9585afd7b92a1bea69747ab496a81008e3059468
avison <avisonlivingstone@gmail.com>
2020-09-09 16:15:24 +0200

    Database Redesign

--- school/users/models.py
+++ school/users/models.py
@@ -2,38 +2,26 @@ from django.db import models
 from django.contrib.auth.models import User 
 from model_utils import Choices
 
-class Senior_Student(models.Model):
-	SUBJECTS = Choices(
-		('Mathematics',_('Mathematics')),
-		('English',_('English Language')),
-		('Physics',_('Physics')),
-		('Chemistry',_('Chemistry')),
-		('Biology',_('Biology')),
-		('Geography',_('Geography')),
-		('Accounts',_('Financial Accounting')),
-		('Commerce',_('Commerce')),
-		('Literature',_('Literature in English')),
-		('FMath',_('Further Mathematics'))
-	)
-	first_name = models.CharField(max_length=150)
-	last_name = models.CharField(max_length=150)
-	username = models.ForeignKey(User, on_delete=models.CASCADE)
+class Student(models.Model):
+	STUDENT_LABEL = Choices(
+		('Senior Class',('Senior Catergory'))
+		('Junior',('Junior Catergory'))
+		)
+	username = models.OneToOneField(User, on_delete=models.CASCADE)
+	first_name = models.CharField(max_length= 100)
+	last_name = models.CharField(max_length= 100)
+	gender = models.CharField(max_length= 2)
 	D_O_B = models.DateTimeField()
 	address = models.CharField(max_length=300)
+	parent_contact = models.IntegerField()
 	student_class = models.IntegerField()
 	student_club = models.CharField(max_length=100)
-	subject = models.CharField(max_length=50, choices=SUBJECTS)
-
+	student_catergory = models.CharField(max_length=50, choices=STUDENT_LABEL)
 
 	def __str__(self):
 		return f'{self.first_name}{self.last_name}'
 
-class Teacher(models.Model):
-	username = models.ManyToManyFields()
 
 
-	class Meta:
-		model = Student
-		fields = ['first_name','last_name','address','subject']
 
 	
