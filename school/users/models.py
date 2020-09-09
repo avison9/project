2852e3b1b4172a7bbaae2ad8a7856c4eee15e474
avison <avisonlivingstone@gmail.com>
2020-09-09 17:36:11 +0200

    db creation

--- school/users/models.py
+++ school/users/models.py
@@ -13,17 +13,17 @@ class Student(models.Model):
 	username = models.OneToOneField(User, on_delete=models.CASCADE)
 	first_name = models.CharField(max_length= 100)
 	last_name = models.CharField(max_length= 100)
-	gender = models.CharField(max_length= 2)
-	D_O_B = models.DateTimeField()
+	gender = models.CharField(max_length= 6)
+	# D_O_B = models.DateTimeField(Null= True)
 	address = models.CharField(max_length=300)
 	parent_contact = models.IntegerField()
-	student_class = models.IntegerField()
+	student_class = models.CharField(max_length= 10)
 	student_club = models.CharField(max_length=100)
 	# student_catergory = models.CharField(max_length=50, choices=STUDENT_LABEL)
 	subject = models.ManyToManyField(Subjects)
 
 	def __str__(self):
-		return f'{self.first_name}{self.last_name}'
+		return f'{self.first_name} {self.last_name}'
 
 class Teacher(models.Model):
 	username = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -33,8 +33,6 @@ class Teacher(models.Model):
 	address = models.CharField(max_length= 100)
 	telephone = models.IntegerField()
 	subject = models.ManyToManyField(Subjects)
-
-
 
 	def __str__(self):
 			return self.first_name
